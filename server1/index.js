0410d4d804ee0e28ba4aa489ecfe120da06b37e8
uhitha18 <uhithakonuganti@gmail.com>
2021-10-06 21:44:54 +0530

    Add zomato proj models

---
+++ server1/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+
+
+const zomato=express();
+
+// application middlewares
+zomato.use(express.json());
+zomato.use(express.urlencoded({extended: false}));
+zomato.use(helmet());
+zomato.use(cors());
+
+
+zomato.get("/",(req,res) => res.json({message:"Set up is successful"}))
+
+zomato.listen(4000,()=> console.log("Server is running successfully"));
